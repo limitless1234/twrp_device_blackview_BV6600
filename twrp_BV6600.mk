a7855f71ef2cc427eb08193a505e76961b836790
limitless1234 <128695592+limitless1234@users.noreply.github.com>
2026-08-21 23:27:37 +0100

    Update twrp_BV6600.mk

--- twrp_BV6600.mk
+++ twrp_BV6600.mk
@@ -10,7 +10,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# $(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from BV6600 device
 $(call inherit-product, device/blackview/BV6600/device.mk)
