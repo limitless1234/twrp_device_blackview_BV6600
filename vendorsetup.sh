1d2c9c3e635d36ff77273d09635d603b3ad4fb51
limitless1234 <128695592+limitless1234@users.noreply.github.com>
2026-08-21 23:00:33 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_BV6600-user
-add_lunch_combo omni_BV6600-userdebug
-add_lunch_combo omni_BV6600-eng
+add_lunch_combo twrp_BV6600-user
+add_lunch_combo twrp_BV6600-userdebug
+add_lunch_combo twrp_BV6600-eng
