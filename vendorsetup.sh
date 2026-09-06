8810cf41fac6f7799f19a497fb7c08a5af8e1842
limitless1234 <128695592+limitless1234@users.noreply.github.com>
2026-09-06 16:46:06 +0100

    Replace add_lunch_combo with COMMON_LUNCH_CHOICES
    Replaced obsolete add_lunch_combo with COMMON_LUNCH_CHOICES

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp_BV6600-user
-add_lunch_combo twrp_BV6600-userdebug
-add_lunch_combo twrp_BV6600-eng
+COMMON_LUNCH_CHOICES twrp_BV6600-user
+COMMON_LUNCH_CHOICES twrp_BV6600-userdebug
+COMMON_LUNCH_CHOICES twrp_BV6600-eng
